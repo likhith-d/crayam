a315906c7723746bad11aa85453b46934ebf14bf
Likhith Draksharam <likhithd@yahoo.com>
2025-03-18 20:01:17 +0530

    Improved Functionality
    -> Added Description
    -> Implemented Front-End login logic

--- Front-End/crayam/src/components/Home.jsx
+++ Front-End/crayam/src/components/Home.jsx
@@ -5,23 +5,35 @@
     export class Home extends Component {
     render() {
         return (
-        <div className="home">
-            <div className="home-container">
-                <div className="home-logo">
-                    <p className="logo-part-1">c</p>
-                    <p className="logo-part-2">rayam</p>
-                    <img src={logo} alt="Crayam Logo" className="logo-image" />
+        <div className="home urbanist">      
+        <div className="home_container">
+            <div className="left">      
+            <div className="home-logo">
+                <p className="logo-part-1">c</p>
+                <p className="logo-part-2">rayam</p>
+                <img src={logo} alt="Crayam Logo" className="logo-image" />
+            </div>
+            <p>Crayam is a next-generation e-commerce platform designed to redefine online shopping with a seamless, customer-centric experience. Offering a diverse range of high-quality products, from trendy fashion and innovative gadgets to home essentials and lifestyle accessories, Crayam brings affordability and convenience under one roof.
+
+
+<br></br> <br></br>
+At Crayam, we believe in blending creativity with commerce, offering exclusive, handpicked collections that cater to every need. Whether you're looking for the latest tech, stylish apparel, or unique home décor, Crayam is your go-to destination for all things trendy and practical.</p>
+            </div>
+            <div className="right">      
+                <div className="login_heading">
+                    Welcome Back !
                 </div>
+                <br></br>
+                <br></br>
+                <div className="right-text">Email :</div>
+                <input type="email" placeholder="crayam@crazy.go"/>
+                <br></br>
+                <div className="right-text">Password :</div>
+                <input type="password" placeholder="top secret"/>
+                <br></br>
+                <button>Login</button>
             </div>
-            <div className="content">
-                Crayam is a next-generation e-commerce platform designed to redefine online shopping with a seamless,
-                customer-centric experience. Offering a diverse range of high-quality products,
-                from trendy fashion and innovative gadgets to home essentials and lifestyle accessories,
-                Crayam brings affordability and convenience under one roof.
-            </div>
-            <div className="big">
-                We Are Launching Soon !
-            </div>
+        </div>
         </div>
         );
     }
