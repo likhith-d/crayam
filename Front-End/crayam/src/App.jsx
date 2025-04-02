e0ebeee67b59111fcdf096d3e99b4ea2785d73e4
Likhith Draksharam <likhithd@yahoo.com>
2025-04-02 12:19:35 +0530

    Smooth Transitions & Pointer

--- Front-End/crayam/src/App.jsx
+++ Front-End/crayam/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
+import Landing from './components/Landing';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/landing" element={<Landing />} />
+                <Route path="*" element={<h1>404 - Page Not Found</h1>} />
             </Routes>
         </Router>
     );
---
+++ Front-End/crayam/src/components/Landing.jsx
@@ -0,0 +1,14 @@
+import "../css/Landing.css";
+import logo from "../assets/crayam.svg";
+
+import React, { Component } from 'react'
+
+export class Landing extends Component {
+  render() {
+    return (
+      <div>Landing</div>
+    )
+  }
+}
+
+export default Landing
